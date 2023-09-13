283e9c6b4f39a1063780708a6c72b5c936d0e802
yujin <yujinsong8226@naver.com>
2023-09-13 14:59:37 +0900

    vcpp최종

--- VCpptest2/VCpptest2/VCpptest2.cpp
+++ VCpptest2/VCpptest2/VCpptest2.cpp
@@ -4,38 +4,50 @@
 using namespace std;
 
 int main() {
-    cout << "♥★♥송유진의 행복한 VC++♥★♥";
+    cout << "♥★♥송유진의 행복한 VC++♥★♥\n" << endl;
 
     char command;
     char cursor = '_';
 
     while (true) {
-        cout << "\ncommand>" << cursor;
+        cout << "command>" << cursor;
+
+        command = _getch(); 
+        
+        
+        if (command == 27) { 
+            cout << "\n프로그램 종료" << endl;
+            break;
+        }
+      
+        cout << command;
+
+        while (true) {
+            char enter = _getch();
+            if (enter == '\r') 
+                break;
+        }
        
-        command = _getch();
-        cout << "\b";
-        cin >> command;
+
 
         switch (command) {
         case 'q':
-            cout << "qwer 화이팅";
+            cout << " : qwer 화이팅" << endl;
             break;
         case 'w':
-            cout << "과제 모르겠다";
+            cout << " : 과제 모르겠다" << endl;
             break;
         case 'e':
-            cout << "담주 부터 3/1배";
+            cout << " : 담주 부터 3/1배" << endl;
             break;
         case 'r':
-            cout << "햅피햅피햅피";
+            cout << " : 햅피햅피햅피" << endl;
             break;
-        case 27:
-            cout << "프로그램 종료" << endl;
-            return 0;
         default:
-            cout << "오류: 알 수 없는 커맨드, 다시 입력하세요.";
+            cout << "\n오류: 알 수 없는 커맨드, 다시 입력하세요." << endl;
             break;
         }
     }
+
     return 0;
 }
